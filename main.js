e73ba543f00756737361b1f04cb15f5b191db945
dasaach <daryazdrav@yandex.ru>
2025-02-04 02:42:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,14 @@ $(document).ready(()=>{
     });
 
 
-
+    $('.open-popup').click(function(e) {
+            e.preventDefault();
+            $('.popup-bg').fadeIn(800);
+        });
+        
+        $('.close-popup').click(function() {
+            $('.popup-bg').fadeOut(800);
+        });
          
     
 
